88e70f5910693cc94979700c3e3a72b43803d77b
Alejandro Mendoza Rosso <alejandromendozarosso@mac.home>
2026-03-17 21:01:07 +0000

    Version 2 Basic Mapping

--- foodSupportProject.py
+++ foodSupportProject.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import re
 import requests
+import folium
+from streamlit_folium import st_folium
 
 # Start with the page config
 
@@ -18,6 +20,13 @@ st.set_page_config(
 # =====
 file_path = "dataset_ae1.csv"
 postcodes_api = "https://api.postcodes.io/postcodes" # use it for lan/lon based on postcode
+
+# =====
+# Defaults for map postioning over east london
+# =====
+default_map_lat = 51.515
+default_map_lon = -0.03
+default_map_zoom = 12
 
 # =====
 # Helper Function
@@ -102,6 +111,84 @@ def fetch_postcodes(postcodes: list[str]) -> dict:
     return lookup
 
 # =====
+# Helper Function
+# Unique locations builder
+# =====
+
+def build_locations_df(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function builds a dataframe of unique locations
+    As the dataset contains multiple row per location,
+    This is essential for mapping
+    """
+    location_cols = [
+        "name",
+        "category",
+        "type",
+        "address",
+        "postcode",
+        "latitude",
+        "longitude"
+    ]
+
+    locations_df = df[location_cols].drop_duplicates().dropna(subset=["latitude", "longitude"])
+
+    return locations_df
+
+# =====
+# Helper Function
+# Marker Color
+# =====
+def get_marker_color(category:str) -> str:
+    """
+    Divides markers on map in colors based on category
+    """
+    if category == "Food Bank":
+        return "red"
+
+    if category == "Food Support":
+        return "green"
+
+    return "blue"
+
+# =====
+# Helper Function
+# Create the Map
+# =====
+def map_creator(locations_df: pd.DataFrame) -> folium.Map:
+    """
+    Creates a basic folium map and maps the coordinates
+    """
+
+    m = folium.Map(
+        location=[default_map_lat, default_map_lon],
+        zoom_start=default_map_zoom,
+        tiles="OpenStreetMap"
+    )
+
+    for index, row in locations_df.iterrows():
+        # for each marker add a list of info
+        popup = f"""
+        <b>{row['name']}</b><br>
+        Category: {row['category']}<br>
+        Type: {row['type']}<br>
+        Address: {row['address']}<br>
+        Postcode: {row['postcode']}
+        """
+
+        color = get_marker_color(row["category"])
+
+        folium.Marker(
+            location=[row["latitude"], row["longitude"]],
+            popup= folium.Popup(popup, max_width=250),
+            tooltip=row["name"],
+            icon=folium.Icon(color=color, icon ="info-sign")
+        ).add_to(m)
+
+    return m
+
+
+# =====
 # Data Loading
 # =====
 @st.cache_data(show_spinner=False) # if the input file does not change this function will not rerun, saving time
@@ -133,9 +220,12 @@ def load_data(file_path: str) -> pd.DataFrame:
 # Main App
 # =====
 st.title("Food Support Accessibility Research")
-st.write("Version 1. df loading and postcode based coordinates")
+st.write("Version 2. Added basic mapping")
 
 df = load_data(file_path)
+
+# Create the df for mapping
+locations_df = build_locations_df(df)
 
 st.subheader("Coordinates quality check")
 
@@ -144,8 +234,15 @@ col1.metric("total rows", len(df))
 col2.metric("rows with lat", df["latitude"].notna().sum())
 col3.metric("rows with lon", df["longitude"].notna().sum())
 
-# Show dataset
+# Show how many unique locations have bene mapped
+st.metric("Total mapped locations", len(locations_df))
 
+# Shwo the map
+st.subheader("Support services map")
+service_map = map_creator(locations_df)
+st_folium(service_map, height= 600, use_container_width=True)
+
+# Show dataset
 st.subheader("Dataset preview")
 st.dataframe(df, use_container_width=True)
 
